2365684b8c84402cdc23881ee6180af2a7f9c8c4
Tiararose Biezetta <92842340+tiararosebiezetta@users.noreply.github.com>
2021-10-31 05:34:34 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,5 @@ telegram_token = $telegram_token
 user_ids = $user_ids
 group_ids = $group_ids
 gclone_para_override = $gclone_para_override" >> "telegram_gcloner/config.ini"
-npm install http-server -g
-http-server -p $PORT &
+python3 clever.py &
 python3 telegram_gcloner/telegram_gcloner.py
